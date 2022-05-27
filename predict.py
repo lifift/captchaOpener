27805d84a5ea6c19b9a6ac14ee0d145931e26094
lifift <you@example.com>
2022-05-27 09:49:21 +0200

    pas encore parfait ...

--- predict.py
+++ predict.py
@@ -22,21 +22,24 @@ for char in list(tmpchars):
 
 model = models.load_model('current_model')
 
-with I.open('index.png','r') as im:
-    imG=1-((np.asarray(ImageOps.grayscale(im)))/255.0)
-    res=model.predict(imG.reshape(1,28,28))
-    mostP=(0,0)
-    for x in range(len(res[0])):
-        if res[0][x]>mostP[1]:
-            mostP=(x,res[0][x])
-    print("le plus probable est le charactère : "+ list(tmpchars)[mostP[0]])
-    print("La proba est : "+str(mostP[1]))
-    plt.imshow(imG,cmap='gray')
-    plt.show()
+# with I.open('index.png','r') as im:
+#     imG=1-((np.asarray(ImageOps.grayscale(im)))/255.0)
+#     res=model.predict(imG.reshape(1,28,28))
+#     mostP=(0,0)
+#     for x in range(len(res[0])):
+#         if res[0][x]>mostP[1]:
+#             mostP=(x,res[0][x])
+#     print("le plus probable est le charactère : "+ list(tmpchars)[mostP[0]])
+#     print("La proba est : "+str(mostP[1]))
+#     plt.imshow(imG,cmap='gray')
+#     plt.show()
 
-liste_images = os.listdir('lettres/')
+
+
+resultat_final=""
+liste_images = os.listdir('lettres_tmp/')
 for name in liste_images:
-    with I.open('lettres/'+name,'r') as im:
+    with I.open('lettres_tmp/'+name,'r') as im:
         imG=1-((np.asarray(ImageOps.grayscale(im)))/255.0)
         res=model.predict(imG.reshape(1,28,28))
         mostP=(0,0)
@@ -45,5 +48,7 @@ for name in liste_images:
                 mostP=(x,res[0][x])
         print("le plus probable est le charactère : "+ list(tmpchars)[mostP[0]])
         print("La proba est : "+str(mostP[1]))
+        resultat_final +=list(tmpchars)[mostP[0]]
         plt.imshow(imG,cmap='gray')
         plt.show()
+print(resultat_final)
